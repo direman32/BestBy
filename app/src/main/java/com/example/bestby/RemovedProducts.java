72703950c469f024772e8b648b248a624ea1e7b2
direman32 <cathalf32@outlook.com>
2020-08-17 13:05:06 +0100

    Writing to excel file
    Generating excel file and writing removed products to it

--- app/src/main/java/com/example/bestby/RemovedProducts.java
+++ app/src/main/java/com/example/bestby/RemovedProducts.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,6 +18,19 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -29,6 +43,7 @@ public class RemovedProducts extends AppCompatActivity {
     private ListView removedProductsView;
     private String userID;
     private View progressOverlay;
+    private List<DocumentSnapshot> myProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +67,7 @@ public class RemovedProducts extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             List<DocumentSnapshot> myListOfDocuments = task.getResult().getDocuments();
+                            myProducts = myListOfDocuments;
                             FillListView(myListOfDocuments);
                         }
                     }
@@ -112,6 +128,64 @@ public class RemovedProducts extends AppCompatActivity {
                 }
         return myListOfDocuments;
     }
+
+
+    public void GenerateReport(View view) {
+//        startActivity(new Intent(getApplicationContext(), Excel.class));
+        Workbook wb=new HSSFWorkbook();
+        Cell cell=null;
+        CellStyle cellStyle=wb.createCellStyle();
+        cellStyle.setFillForegroundColor(HSSFColor.WHITE.index);
+        //cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+
+        //Now we are creating sheet
+        Sheet sheet=null;
+        sheet = wb.createSheet("Removed Products");
+        //Now column and row
+
+
+        myProducts = SortByDate(myProducts);
+        for(int i = 0; i < myProducts.size(); i++) {
+            Row row = sheet.createRow(i);
+
+            cell = row.createCell(0);
+            cell.setCellValue(myProducts.get(i).get(staticValues.PRODUCT_NAME_KEY).toString().trim());
+            cell.setCellStyle(cellStyle);
+
+            cell = row.createCell(1);
+            cell.setCellValue(myProducts.get(i).get("numberRemoved").toString().trim());
+            cell.setCellStyle(cellStyle);
+
+            cell = row.createCell(2);
+            cell.setCellValue(myProducts.get(i).get(staticValues.PRODUCT_DATE_DISPLAY_KEY).toString().trim());
+            cell.setCellStyle(cellStyle);
+        }
+
+        sheet.setColumnWidth(0, 8000);
+        sheet.setColumnWidth(1, 2000);
+        sheet.setColumnWidth(2, 3000);
+
+        File file = new File(getExternalFilesDir(null),"WeeklyReport.xls");
+        FileOutputStream outputStream =null;
+
+        try {
+            outputStream=new FileOutputStream(file);
+            wb.write(outputStream);
+            Toast.makeText(getApplicationContext(),"OK",Toast.LENGTH_LONG).show();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+
+            Toast.makeText(getApplicationContext(),"NO OK",Toast.LENGTH_LONG).show();
+            try {
+                outputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
 
     public void setInvisible() {
         progressOverlay.setVisibility(View.INVISIBLE);
